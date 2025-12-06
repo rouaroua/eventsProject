d48d327735fc02ac7f627b55539680e1f9e2cb71
rouaroua <rouakharrat662002@gmail.com>
2025-12-06 21:13:10 +0100

    add h2 test configuration for jenkins

--- src/test/java/tn/fst/eventsproject/EventsProjectApplicationTests.java
+++ src/test/java/tn/fst/eventsproject/EventsProjectApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventsProjectApplicationTests {
 
     @Test
-    void contextLoads() {
+   void contextLoads() {
     }
 
 }
